5bf4144c1bfe5dedb44c4b65d85fb1f55a4b7786
yoshi08010801 <207516019+yoshi08010801@users.noreply.github.com>
2026-08-20 15:23:41 +0900

    chore: remove unused empty Swift files

--- File.swift
+++
@@ -1,8 +0,0 @@
-//
-//  File.swift
-//  VoiceTranslateMVP
-//
-//  Created by k on 2025/12/09.
-//
-
-import Foundation
--- ViewModels/Untitled.swift
+++
@@ -1,7 +0,0 @@
-//
-//  Untitled.swift
-//  VoiceTranslateMVP
-//
-//  Created by k on 2025/12/09.
-//
-
